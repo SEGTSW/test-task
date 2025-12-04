68b8fc370bd57a87214ce320d7445808115e4997
Max web2025-4 <denderys32@gmail.com>
2025-12-04 21:58:57 +0200

    2

--- main.js
+++ main.js
@@ -59,9 +59,9 @@ function showExample1() {
     </header>
     <nav>
         <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a >Home</a></li>
+            <li><a >About</a></li>
+            <li><a >Contact</a></li>
         </ul>
     </nav>
     <main>
